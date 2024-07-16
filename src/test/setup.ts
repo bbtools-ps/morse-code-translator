ae9ed4038426360c019f237e8e8e6ab86461fb0b
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2024-07-16 15:02:39 +0200

    updated testing library, typescript and vitest

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,11 +1,7 @@
-import matchers from "@testing-library/jest-dom/matchers";
+import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach, expect, vi } from "vitest";
+import { afterEach, vi } from "vitest";
 
-// extends Vitest's expect method with methods from react-testing-library
-expect.extend(matchers);
-
-// runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
 });
