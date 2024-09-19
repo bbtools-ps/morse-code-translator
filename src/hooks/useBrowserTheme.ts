ec90761c664a99f89a62815b09135f1d21aeb06c
Bogdan Bogdanovic <67063384+bbtools-ps@users.noreply.github.com>
2024-09-19 23:08:30 +0200

    added import type rule

--- src/hooks/useBrowserTheme.ts
+++ src/hooks/useBrowserTheme.ts
@@ -6,6 +6,7 @@ export const useBrowserTheme = () => {
   );
 
   useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleThemeChange = (e: any) => {
       setIsDarkTheme(e.matches);
     };
